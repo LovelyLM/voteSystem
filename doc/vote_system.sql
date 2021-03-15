03b5f09aff118b03dbb8b51dab3cbfdb177f87aa
lovelylm <leiming520vip@gmail.com>
2021-03-16 00:22:31 +0800

    添加了数据库文件

--- doc/vote_system.sql
+++ doc/vote_system.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 50726
+ Source Server Version : 80022
  Source Host           : localhost:3306
  Source Schema         : vote_system
 
  Target Server Type    : MySQL
- Target Server Version : 50726
+ Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 27/06/2019 13:25:48
+ Date: 16/03/2021 00:22:04
 */
 
 SET NAMES utf8mb4;
@@ -22,11 +22,11 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(0) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of admin
@@ -34,13 +34,51 @@ CREATE TABLE `admin`  (
 INSERT INTO `admin` VALUES (1, 'leiming', '123456');
 
 -- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `courseName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `credit` int(0) NULL DEFAULT NULL,
+  `period` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of course
+-- ----------------------------
+INSERT INTO `course` VALUES (2, '数学', 2, 45);
+INSERT INTO `course` VALUES (3, '语文', 3, 32);
+
+-- ----------------------------
+-- Table structure for grade
+-- ----------------------------
+DROP TABLE IF EXISTS `grade`;
+CREATE TABLE `grade`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `courseId` int(0) NULL DEFAULT NULL,
+  `grade` int(0) NULL DEFAULT NULL,
+  `studentId` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of grade
+-- ----------------------------
+INSERT INTO `grade` VALUES (1, 2, 6, 1);
+INSERT INTO `grade` VALUES (2, 3, 7, 1);
+INSERT INTO `grade` VALUES (3, 2, 8, 2);
+INSERT INTO `grade` VALUES (4, 3, 9, 2);
+
+-- ----------------------------
 -- Table structure for player
 -- ----------------------------
 DROP TABLE IF EXISTS `player`;
 CREATE TABLE `player`  (
-  `id` int(1) NOT NULL AUTO_INCREMENT,
+  `id` int(0) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `age` int(1) NULL DEFAULT NULL,
+  `age` int(0) NULL DEFAULT NULL,
   `gender` varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `telephone` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `words` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -52,16 +90,35 @@ CREATE TABLE `player`  (
 -- ----------------------------
 INSERT INTO `player` VALUES (27, '周杰伦', 40, '男', '18884512316', NULL);
 INSERT INTO `player` VALUES (30, '雷鸣', 22, '女', '18883653609', NULL);
-INSERT INTO `player` VALUES (32, '蔡依林', 38, '男', '17778456453', NULL);
 INSERT INTO `player` VALUES (33, '林俊杰', 40, '男', '18874569423', NULL);
 INSERT INTO `player` VALUES (36, '曹东升', 22, '男', '16664587123', NULL);
+INSERT INTO `player` VALUES (37, '朴素妍', 27, '女', '15784564544', NULL);
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `stuNo` int(0) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `className` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `sumGrade` int(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (1, 110, '周杰伦', '三年二班', 99);
+INSERT INTO `student` VALUES (2, 111, '蔡依林', '三年二班', 98);
 
 -- ----------------------------
 -- Table structure for vote
 -- ----------------------------
 DROP TABLE IF EXISTS `vote`;
 CREATE TABLE `vote`  (
-  `vid` int(1) NOT NULL AUTO_INCREMENT,
+  `vid` int(0) NOT NULL AUTO_INCREMENT,
   `vdescribe` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `vplayer` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `vpoll` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
@@ -75,6 +132,7 @@ CREATE TABLE `vote`  (
 -- ----------------------------
 -- Records of vote
 -- ----------------------------
-INSERT INTO `vote` VALUES (6, '全国第一届帅哥大赛', '肖东升,辜鸿铭,雷鸣', '51,88,96', 1, '2019-06-23 22:04:46.854715', 0, ',119.86.233.75,119.86.237.75');
+INSERT INTO `vote` VALUES (6, '全国第一届帅哥大赛', '肖东升,辜鸿铭,雷鸣', '51,88,96', 0, '2019-06-23 22:04:46.854715', 0, ',119.86.233.75,119.86.237.75');
+INSERT INTO `vote` VALUES (7, '美女比赛', '朴素妍,雷鸣,曹东升', '1,0,0', 1, '2021-03-01 23:58:26.750823', 0, ',125.70.31.58');
 
 SET FOREIGN_KEY_CHECKS = 1;
